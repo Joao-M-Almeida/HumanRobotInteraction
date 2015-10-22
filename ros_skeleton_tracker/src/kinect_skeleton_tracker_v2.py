31c391de2faf7cedb4d03370f778fe3dacc3961a
fjccosta <filipe.correia.costa@gmail.com>
2015-10-22 12:05:27 +0100

    22Out2015 - updated ros_skeleton_tracker_v2 to gather all points and added some test examples for opencv

--- ros_skeleton_tracker/src/kinect_skeleton_tracker_v2.py
+++ ros_skeleton_tracker/src/kinect_skeleton_tracker_v2.py
@@ -13,22 +13,39 @@ import geometry_msgs.msg
 
 BASE_FRAME = '/openni_depth_frame'
 FRAMES = [
-        'head',
-        'neck',
-        'torso',
-        'left_shoulder',
-        'left_elbow',
-        'left_hand',
-        'left_hip',
-        'left_knee',
-        'left_foot',
-        'right_shoulder',
-        'right_elbow',
-        'right_hand',
-        'right_hip',
-        'right_knee',
-        'right_foot',
+        '/head',
+        '/neck',
+        '/torso',
+        '/left_shoulder',
+        '/left_elbow',
+        '/left_hand',
+        '/left_hip',
+        '/left_knee',
+        '/left_foot',
+        '/right_shoulder',
+        '/right_elbow',
+        '/right_hand',
+        '/right_hip',
+        '/right_knee',
+        '/right_foot'
         ]
+Coord = [
+		[0.0,0.0,0.0],
+		[0.0,0.0,0.0],
+		[0.0,0.0,0.0],
+		[0.0,0.0,0.0],
+		[0.0,0.0,0.0],
+		[0.0,0.0,0.0],
+		[0.0,0.0,0.0],
+		[0.0,0.0,0.0],
+		[0.0,0.0,0.0],
+		[0.0,0.0,0.0],
+		[0.0,0.0,0.0],
+		[0.0,0.0,0.0],
+		[0.0,0.0,0.0],
+		[0.0,0.0,0.0],
+		[0.0,0.0,0.0]
+		]
         
 if __name__ == '__main__':
     rospy.init_node('kinect_tracking')
@@ -37,12 +54,31 @@ if __name__ == '__main__':
 
     rate = rospy.Rate(1)
     while not rospy.is_shutdown():
+    	
+    	for master in range(1,10):
+			try:
+				st='/head_' + str(master)
+				#print(listener.lookupTransform(BASE_FRAME, st, rospy.Time(0)))
+				break
+			except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+   				continue
+   		
+    	
+    	print(master)		
+    	
     	try:
         	#(trans,rot) = listener.lookupTransform('camera_link', '/camera_rgb_frame', rospy.Time(0))
             #(trans,rot) = listener.lookupTransform('/openni_depth_frame', '/head_3', rospy.Time(0))
-            print(listener.lookupTransform('/openni_depth_frame', '/head_1', rospy.Time(0)))
+			for f in range(0,len(FRAMES)):
+				st='_' + str(master)
+				coordinates = listener.lookupTransform(BASE_FRAME, FRAMES[f] + st, rospy.Time(0))
+				Coord[f] = coordinates[0]
+            	
+            	
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
         	continue
+        
+        print(Coord)
         
 		#print("Head_:" + trans + rot)
         
