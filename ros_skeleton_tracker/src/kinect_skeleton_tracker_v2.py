0ba06d2a2254e9d6fd2612fce1b4a83af5bc53e3
fjccosta <filipe.correia.costa@gmail.com>
2015-10-26 14:52:56 +0000

    skeleton tracker tem issue de só conseguir fazer tracking a um user predeterminado, aadded initial test code for opencv

--- ros_skeleton_tracker/src/kinect_skeleton_tracker_v2.py
+++ ros_skeleton_tracker/src/kinect_skeleton_tracker_v2.py
@@ -11,6 +11,11 @@ import tf
 import math
 import geometry_msgs.msg
 
+#debug stuff
+import curses
+import atexit
+
+
 BASE_FRAME = '/openni_depth_frame'
 FRAMES = [
         '/head',
@@ -46,43 +51,68 @@ Coord = [
 		[0.0,0.0,0.0],
 		[0.0,0.0,0.0]
 		]
+
+stdscr = curses.initscr()
+
+
+def exit_handler():
+    curses.endwin()
+    print 'Exiting!'
+
+def print_coord():
+	for f in range(0,len(FRAMES)):
+		string=str(FRAMES[f]) + '\t->\t' + str(['%.6f' % elem for elem in Coord[f]]) + '\t'
+		stdscr.addstr(f, 0, string)
+		#print FRAMES[f] , '->', Coord[f]
+	
+	stdscr.refresh()
         
 if __name__ == '__main__':
-    rospy.init_node('kinect_tracking')
+	stdscr = curses.initscr()
+	print_coord()
+	atexit.register(exit_handler)
+	rospy.init_node('kinect_tracking')
 
-    listener = tf.TransformListener()
+	listener = tf.TransformListener()
 
-    rate = rospy.Rate(1)
-    while not rospy.is_shutdown():
-    	
-    	for master in range(1,17):
-			st='/head_' + str(master)
-			if(listener.waitForTransform(BASE_FRAME, st, rospy.Time(), rospy.Duration(0.25))):
-				break
-			else:
-				continue
+	rate = rospy.Rate(10)
+	while not rospy.is_shutdown():
+		#for master in range(1,17):
+			#st='/torso_' + str(master)
+
+			#try:
+				#found=listener.waitForTransform(BASE_FRAME, st, rospy.Time(), rospy.Duration(0.2))
+			#except tf.Exception as e:
+				#print "some tf exception happened", e.args
+				#continue
+
+			#if found:
+				#break
+			#else:
+				#continue
 			#try:
 				#print(listener.lookupTransform(BASE_FRAME, st, rospy.Time(0)))
 				#break
 			#except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-   				#continue
-   		
-    	if master == 16:
-			print('No master found')
-			continue
-    	print(master)		
-    	
-    	try:
-        	for f in range(0,len(FRAMES)):
+				#continue
+
+		#if master == 16:
+			#print('No master found')
+			#continue
+		
+		master=1
+		#print(master)		
+
+		try:
+			for f in range(0,len(FRAMES)):
 				st='_' + str(master)
 				coordinates = listener.lookupTransform(BASE_FRAME, FRAMES[f] + st, rospy.Time(0))
 				Coord[f] = coordinates[0]
-            	
-        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-        	continue
-        
-        print(Coord)
-        
+		except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+			continue
+
+		print_coord()
+
 		#print("Head_:" + trans + rot)
-        
-        rate.sleep()
+
+		rate.sleep()
