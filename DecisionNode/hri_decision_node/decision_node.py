ce355f532becf7e07653fcb2db926cb949d05e15
Joao-M-Almeida <jmirandadealme@gmail.com>
2015-12-05 12:54:37 +0000

    updated parts of the decision node

--- DecisionNode/hri_decision_node/decision_node.py
+++ DecisionNode/hri_decision_node/decision_node.py
@@ -8,28 +8,56 @@ from geometry_msgs.msg import Pose2D
 from nav_msgs.msg import Odometry
 import threading
 import atexit
+import pandas as pd
+from sklearn.svm import SVC
+from sklearn.decomposition import PCA
+import numpy as np
 
 '''
-    wave = 0
-    low_five = 1
-    high_five = 2
-    clap = 3
+    nop = 1
+    wave = 2
+    h5 = 3
+    l5 = 4
+    go = 5
+    grab = 6
+    go_back = 7
     '''
-alive=True
-n_cmds = 4
+alive = True
+n_cmds = 7
 #prob_cmd = {}
 threshold = 0.8
 default_prob = 1.0/n_cmds
+done = 1
 
-master_position = [(0,0), (0, 0), (0, 0), (0, 0), (0, 0)]
+master_position = [(0, 0), (0, 0), (0, 0), (0, 0), (0, 0)]
 
-commands = {'wave': default_prob, 'low_five': default_prob, 'high_five': default_prob, 'clap': default_prob}
+commands = {1: default_prob, 2: default_prob, 3: default_prob, 4: default_prob, 5: default_prob, 6: default_prob, 7: default_prob}
 
 kat_pub = rospy.Publisher('/katana_commands', String, queue_size=10)
 scout_pub = rospy.Publisher('/scout_commands', Pose2D, queue_size=10)
 
 kinect_info = []
 
+pca = PCA(n_components = 15)
+classf = SVC()
+
+def train_classifier():
+
+    global pca
+    global classf
+
+    database = pd.read_csv( "/home/jmirandadealme/Documents/SistAut/HumanRobotInteraction/others/TrainingData/todos2.txt", quotechar ="'")
+
+    labels = database['label'].values
+    database.drop("label",axis=1,inplace=True)
+    features = database.values
+
+    pca.fit(features)
+    features = pca.transform(features)
+
+    classf.fit(features,labels)
+
+'''
 def new_gesture(gesture):
     msg = String()
     msg.data = 'wave'
@@ -37,7 +65,11 @@ def new_gesture(gesture):
         kat_pub.publish(msg)
         rospy.loginfo("Sending to Katana"+str(msg))
     kinect_info.append((gesture.gesture,gesture.header.stamp.secs))
+'''
 
+'''
+At each new pose msg, update internal master position
+'''
 def new_position(pose):
     master_position.pop(0)
     master_position.append((pose.x,pose.y))
@@ -57,7 +89,7 @@ def action_controller():
         #rospy.loginfo("Kinect gestures: " + str(kinect_info))
         #rospy.loginfo("Master: " + str(master_position))
         for cmd, prob in commands.iteritems():
-            if prob > threshold and done==1:
+            if prob > threshold and done == 1:
                 rospy.loginfo("Executing: " + str(cmd))
                 commands[cmd] = default_prob
                 normallize_cmd_prob()
@@ -65,8 +97,11 @@ def action_controller():
 
         rate.sleep()
 
+'''
+At each new odom msg, update internal scout position and rotation
+'''
 def odom_process(data):
-    quaternion=[0,0,0,0]
+    quaternion=[0, 0, 0, 0]
     global rpy
     global scout_x
     global scout_y
@@ -85,7 +120,7 @@ def feedback(str_a):
     global done
     done = 1
 
-
+'''
 def new_movement(data):
     moveit = data.movement
     msg.data = ''
@@ -120,6 +155,22 @@ def new_movement(data):
         #publisher do scout
         scout_pub.publish(msg)
         rospy.loginfo("Sending to Scout"+str(msg))
+'''
+
+'''
+At each new feature vector from the kinect predict which action the robot should
+do, use that to update the probabilities for each command.
+'''
+def new_feature(feature_str):
+    feature = np.fromstring(feature_str.data,dtype = float , sep = ',')
+    new_sample = pca.transform(feature)
+    predicted_action = classf.predict(sample)
+    rospy.loginfo("Received : \n" + feature_str.data + "\n Predicted: " + str(predicted_action))
+    for cmd, prob in commands.iteritems():
+        if cmd == predicted_action:
+            commands[cmd] = prob + increment
+    normallize_cmd_prob()
+
 
 
 def goodbye():
@@ -129,11 +180,18 @@ if __name__ == '__main__':
     atexit.register(goodbye)
 
     try:
+
+        train_classifier()
+
         rospy.init_node('hri_decision_node', anonymous=True)
-        #rospy.Subscriber('/gestures', gesture, new_gesture)
+
         rospy.Subscriber('/masterlocation', pose_msg, new_position)
-        rospy.Subscriber('/movements', String, new_movement)
         rospy.Subscriber('/odom', Odometry, odom_process)
+
+#        rospy.Subscriber('/movements', String, new_movement)
+
+        rospy.Subscriber('/feature_vector', String, new_feature)
+
         rospy.Subscriber('/actionfeedback', String, feedback)
 
         rate = rospy.Rate(0.5)
